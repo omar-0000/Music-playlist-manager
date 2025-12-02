99c0ab0c85dc9cb4df575b700ac4ce72dcbe3720
omar <Omarobaid31452@gmail.com>
2025-12-02 02:33:31 +0000

    Add files via upload

--- MusicPlayer/Program.cs
+++ MusicPlayer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace MusicPlaylist
 {
     public class Song
@@ -8,7 +9,7 @@ namespace MusicPlaylist
         public string Album;
         public int DurationInSeconds;
         public string Genre;
-        public Song Next; // pointer for linked list
+        public Song Next;
 
         public Song(string artist, string title, string album, int durationInSeconds, string genre)
         {
@@ -25,16 +26,18 @@ namespace MusicPlaylist
             return $"{Title} by {Artist} | Album: {Album} | Duration: {DurationInSeconds}s | Genre: {Genre}";
         }
     }
+
     public class Playlist
     {
         public Song head;
-        private Song current; 
+        private Song current;
+
         public Playlist()
         {
             head = null;
             current = null;
         }
-        // add a song which will be from the csv
+
         public void AddSong(Song newSong)
         {
             if (head == null)
@@ -47,7 +50,7 @@ namespace MusicPlaylist
                 temp = temp.Next;
             temp.Next = newSong;
         }
-        // printsall songs 
+
         public void PrintAllSongs()
         {
             Song temp = head;
@@ -71,13 +74,11 @@ namespace MusicPlaylist
                 Console.WriteLine($"Deleted song: {title}");
                 return;
             }
-
             Song temp = head;
             while (temp.Next != null && !temp.Next.Title.Equals(title, StringComparison.OrdinalIgnoreCase))
             {
                 temp = temp.Next;
             }
-
             if (temp.Next == null)
             {
                 Console.WriteLine($"Song with title '{title}' not found.");
@@ -88,6 +89,7 @@ namespace MusicPlaylist
                 Console.WriteLine($"Deleted song: {title}");
             }
         }
+
         public int SearchByTitle(string title)
         {
             Song temp = head;
@@ -97,19 +99,18 @@ namespace MusicPlaylist
             {
                 if (temp.Title.Equals(title, StringComparison.OrdinalIgnoreCase))
                 {
-                    return index;  // found
+                    return index;
                 }
-
                 temp = temp.Next;
                 index++;
             }
 
-            return -1; // not found
+            return -1;
         }
 
         public void AddSongManually()
         {
-                       Console.Write("Enter song title: ");
+            Console.Write("Enter song title: ");
             string title = Console.ReadLine();
             Console.Write("Enter artist name: ");
             string artist = Console.ReadLine();
@@ -119,14 +120,16 @@ namespace MusicPlaylist
             int durationInSeconds = int.Parse(Console.ReadLine());
             Console.Write("Enter genre: ");
             string genre = Console.ReadLine();
+
             Song newSong = new Song(artist, title, album, durationInSeconds, genre);
             AddSong(newSong);
-            Console.WriteLine($"Added song: {newSong.ToString()}");
+
+            Console.WriteLine($"Added song: {newSong}");
         }
 
         public void DeleteSongByPosition(int position)
         {
-            if head == null)
+            if (head == null)
             {
                 Console.WriteLine("Playlist is empty.");
                 return;
@@ -162,9 +165,154 @@ namespace MusicPlaylist
                 temp.Next = temp.Next.Next;
             }
         }
-    }
-    class Program
 
+        public void Shuffle()
+        {
+            List<Song> songs = new List<Song>();
+            Song temp = head;
+
+            while (temp != null)
+            {
+                songs.Add(temp);
+                temp = temp.Next;
+            }
+
+            if (songs.Count == 0)
+            {
+                Console.WriteLine("Playlist is empty.");
+                return;
+            }
+
+            Random rnd = new Random();
+            for (int i = songs.Count - 1; i > 0; i--)
+            {
+                int j = rnd.Next(i + 1);
+                (songs[i], songs[j]) = (songs[j], songs[i]);
+            }
+
+            head = songs[0];
+            temp = head;
+
+            for (int i = 1; i < songs.Count; i++)
+            {
+                temp.Next = songs[i];
+                temp = temp.Next;
+            }
+
+            temp.Next = null;
+            Console.WriteLine("Playlist shuffled.");
+        }
+
+        public void SortByArtist()
+        {
+            List<Song> songs = new List<Song>();
+            Song temp = head;
+
+            while (temp != null)
+            {
+                songs.Add(temp);
+                temp = temp.Next;
+            }
+
+            if (songs.Count == 0)
+            {
+                Console.WriteLine("Playlist is empty.");
+                return;
+            }
+
+            songs.Sort((a, b) => a.Artist.CompareTo(b.Artist));
+
+            head = songs[0];
+            temp = head;
+
+            for (int i = 1; i < songs.Count; i++)
+            {
+                temp.Next = songs[i];
+                temp = temp.Next;
+            }
+
+            temp.Next = null;
+            Console.WriteLine("Playlist sorted by artist.");
+        }
+
+        public void SortByDuration()
+        {
+            List<Song> songs = new List<Song>();
+            Song temp = head;
+
+            while (temp != null)
+            {
+                songs.Add(temp);
+                temp = temp.Next;
+            }
+
+            if (songs.Count == 0)
+            {
+                Console.WriteLine("Playlist is empty.");
+                return;
+            }
+
+            songs.Sort((a, b) => a.DurationInSeconds.CompareTo(b.DurationInSeconds));
+
+            head = songs[0];
+            temp = head;
+
+            for (int i = 1; i < songs.Count; i++)
+            {
+                temp.Next = songs[i];
+                temp = temp.Next;
+            }
+
+            temp.Next = null;
+            Console.WriteLine("Playlist sorted by duration.");
+        }
+
+        public void PlayNext()
+        {
+            if (current == null)
+                current = head;
+            else
+                current = current.Next;
+
+            if (current != null)
+                Console.WriteLine($"Now playing: {current}");
+            else
+                Console.WriteLine("End of playlist reached.");
+        }
+
+        public void PlayPrevious()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Playlist is empty.");
+                return;
+            }
+
+            if (current == null)
+            {
+                current = head;
+                Console.WriteLine($"Now playing: {current}");
+                return;
+            }
+
+            if (current == head)
+            {
+                Console.WriteLine("Already at the beginning of the playlist.");
+                return;
+            }
+
+            Song temp = head;
+            while (temp.Next != current)
+            {
+                temp = temp.Next;
+            }
+
+            current = temp;
+            Console.WriteLine($"Now playing: {current}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
@@ -172,7 +320,6 @@ namespace MusicPlaylist
 
             Playlist playlist = new Playlist();
 
-            // loads songs from CSV
             ReadCSV.LoadSongs("songs_dataset.csv", playlist);
 
             bool exit = false;
@@ -180,77 +327,55 @@ namespace MusicPlaylist
             while (!exit)
             {
                 Console.WriteLine("\n--- Music Playlist Menu ---");
-                Console.WriteLine("1. Display all songs");
-                Console.WriteLine("2. Add a new song");
-                Console.WriteLine("3. Delete a song by title");
-                Console.WriteLine("4. Delete a song by position");
-                Console.WriteLine("5. Display songs sorted by artist");
-                Console.WriteLine("6. Display songs sorted by duration");
-                Console.WriteLine("7. Search for a song by title");
-                Console.WriteLine("8. Shuffle playlist");
-                Console.WriteLine("9. Exit");
-                Console.Write("Enter your choice: ");
+                Console.WriteLine("1. Play next song");
+                Console.WriteLine("2. Play previous song");
+                Console.WriteLine("3. Display all songs");
+                Console.WriteLine("4. Search for a song");
+                Console.WriteLine("5. Sort by artist");
+                Console.WriteLine("6. Sort by duration");
+                Console.WriteLine("7. Shuffle playlist");
+                Console.WriteLine("8. Add a new song");
+                Console.WriteLine("9. Delete by title");
+                Console.WriteLine("10. Delete by position");
+                Console.WriteLine("11. Exit");
 
+                Console.Write("Enter your choice: ");
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
-                    case "1":
-                        playlist.PrintAllSongs();
-                        break;
-
-                    case "2":
-                        // in here will go add song feature
-                        playlist.AddSongManually();
-                        break;
-
-                    case "3":
-                        Console.Write("Enter song title to delete: ");
-                        string delTitle = Console.ReadLine();
-                        playlist.DeleteSongByTitle(delTitle);
-                        break;
-
+                    case "1": playlist.PlayNext(); break;
+                    case "2": playlist.PlayPrevious(); break;
+                    case "3": playlist.PrintAllSongs(); break;
                     case "4":
-                        Console.Write("Enter song position to delete: ");
-                        int delPos = int.Parse(Console.ReadLine());
-                        playlist.DeleteSongByPosition(delPos);
+                        Console.Write("Enter song title: ");
+                        string s = Console.ReadLine();
+                        int pos = playlist.SearchByTitle(s);
+                        Console.WriteLine(pos == -1 ? "Not found" : $"Found at position {pos}");
                         break;
-
-                    case "5":
-                        Console.WriteLine("Display sorted by artist feature will go here.");
-                        break;
-
-                    case "6":
-                        Console.WriteLine("Display sorted by duration feature will go here.");
-                        break;
-
-                    case "7":
-                        Console.Write("Enter song title to search: ");
-                        string searchTitle = Console.ReadLine();
-                        int pos = playlist.SearchByTitle(searchTitle);
-
-                        if (pos == -1)
-                            Console.WriteLine("Song not found.");
-                        else
-                            Console.WriteLine($"Song found at position {pos}.");
-                        break;
-
-                    case "8":
-                        Console.WriteLine("Shuffle feature will go here.");
-                        break;
-
+                    case "5": playlist.SortByArtist(); break;
+                    case "6": playlist.SortByDuration(); break;
+                    case "7": playlist.Shuffle(); break;
+                    case "8": playlist.AddSongManually(); break;
                     case "9":
-                        exit = true;
+                        Console.Write("Enter title: ");
+                        playlist.DeleteSongByTitle(Console.ReadLine());
                         break;
+                    case "10":
+                        Console.Write("Enter position: ");
+                        playlist.DeleteSongByPosition(int.Parse(Console.ReadLine()));
+                        break;
+                    case "11": exit = true; break;
+                    default: Console.WriteLine("Invalid option."); break;
+                }
 
-                    default:
-                        Console.WriteLine("Invalid choice, try again.");
-                        break;
+                if (!exit)
+                {
+                    Console.WriteLine("\nPress Enter to continue...");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
             }
-
-            Console.WriteLine("Exiting Music Playlist Manager.");
         }
     }
-
 }
